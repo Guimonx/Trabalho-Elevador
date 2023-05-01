9b339e37ef1cf59d3a31b6a53e24ba9d982355d1
Guimonx <88748861+Guimonx@users.noreply.github.com>
2023-05-01 12:18:55 -0300

    Commit-do-projeto
    Commit do projeto do elevador

---
+++ TrabalhoElevador/src/scriptspog/Chamadas.java
@@ -0,0 +1,71 @@
+package scriptspog;
+
+public class Chamadas {
+
+	int AndarDestino;
+	int AndarAtual;
+	boolean pedido = false;
+	boolean Elevador_Disponivel;
+	
+	
+	
+	public Chamadas() {
+		
+		AndarDestino = 0;
+		Elevador_Disponivel = true;
+	}
+	
+	public void Liberado() {
+		AndarAtual = AndarDestino;
+		
+		Elevador_Disponivel = true;
+	//	System.out.println(" Disponibilidade do elevador " + Elevador_Disponivel + " valor andaratual " + AndarAtual);
+	}
+	
+	public void MudarAndarDestino(int a) {
+	
+
+		if(Elevador_Disponivel == true)
+		{
+		 AndarDestino = a;
+		}
+		Elevador_Disponivel = false;
+	}
+	
+//	public void MudarAndarDestino(int a) {
+//		
+//		if(Elevador_Disponivel == true)
+//		{
+//		 AndarDestino = a;
+//		}
+//		Elevador_Disponivel = false;
+//	}
+	
+	public void MudarAndarAtual(int b) {
+		
+		AndarDestino = b;
+		
+		Elevador_Disponivel = false;
+	}
+	
+	public int PegarAndarDestino() {
+		
+		Elevador_Disponivel = false;
+		
+		return AndarDestino;
+	}
+	
+	public int PegarAndarAtual() {
+		
+		Elevador_Disponivel = false;
+		
+		return AndarAtual;
+	}
+
+	public boolean EstaDisponivel() {
+		
+		return Elevador_Disponivel;
+	}
+	
+	
+}
